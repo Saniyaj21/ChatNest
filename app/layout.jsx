8a464e01d34521437c0ae967f62ac135ccb2ce36
Saniyaj Mallik <saniyajmallik12@gmail.com>
2025-05-18 20:51:51 +0530

    Auth added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,23 +1,10 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
+  ClerkProvider
 } from '@clerk/nextjs'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
